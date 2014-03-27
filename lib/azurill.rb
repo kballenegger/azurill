91ecdf4d261835770fdaa0e5f851b8bf5744cee2
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-03-26 18:21:33 -0700

    base application

--- lib/azurill.rb
+++ lib/azurill.rb
@@ -1,5 +1,79 @@
 require 'azurill/version'
+require 'ffi-ncurses'
 
 module Azurill
-  # Your code goes here...
+
+  class Application
+
+    # Start the application!
+    #
+    def self.start!
+      # create application instance
+      @current = self.new
+      @current.run!
+    ensure
+      @current.close!
+    end
+
+    # Exits.
+    #
+    def self.exit!
+      throw :exit
+    end
+
+    # Global shared application.
+    #
+    def self.current
+      @current
+    end
+
+
+    # -- actual implementation
+
+    # Create the application, this initializes ncurses, but does not yet do
+    # anything. Call the `run!` method to start the run loop.
+    #
+    def initialize
+      FFI::NCurses.initscr
+      FFI::NCurses.clear
+    end
+
+    # This creates and starts the run loop, this is the root method of the
+    # entire application's lifetime.
+    #
+    def run!
+      @queue = []
+      @controller = Controller.new(self)
+      catch :exit do
+        while true
+          tick
+          FFI::NCurses.refresh
+        end
+      end
+    end
+
+    # This closes the app, and clears the app from the terminal.
+    #
+    def close!
+      @controller.close!
+      FFI::NCurses.endwin
+    end
+
+    # This method gets executed for every iteration of the run loop.
+    #
+    def tick
+      unless queue.empty?
+        @queue.pop.call
+      else
+        # TODO: event handling
+        FFI::NCurses.getch
+      end
+    end
+
+    # Queue a block for the next available tick.
+    #
+    def queue(&b)
+      @queue << b
+    end
+  end
 end
---
+++ lib/azurill/controller.rb
@@ -0,0 +1,15 @@
+
+module Azurill
+  
+  class Controller
+
+    # Initializes the controller with a reference to the application
+    def initialize(app)
+      # :)
+    end
+
+    def close!
+      # TODO close any other threads
+    end
+  end
+end
