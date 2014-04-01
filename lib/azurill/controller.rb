a6866486c130713518455a975eb76c535ffdbcc4
Kenneth Ballenegger <kenneth@ballenegger.com>
2014-04-01 13:12:39 -0700

    cleanups

--- lib/azurill/controller.rb
+++ lib/azurill/controller.rb
@@ -9,7 +9,7 @@ module Azurill
     def initialize
       h, w = FFI::NCurses.getmaxyx(FFI::NCurses.stdscr)
 
-      @main_view = View.new({x: 0, y: 0, w: w, h: h - 1})
+      @main_view = View.new({x: 0, y: 0, w: w, h: h - 2})
 
       @logs = []
 
@@ -17,44 +17,10 @@ module Azurill
       @main_view.draw do
         # first a line at the top
         FFI::NCurses.clear
-        FFI::NCurses.move(rect[:y], rect[:x])
-        top_bar = '[ session 1 ]'
-        top_bar << (rect[:w] - top_bar.length).times.map {|_| ' ' }.join('')
-        FFI::NCurses.attr_set(FFI::NCurses::A_BOLD, Colors.black_on_magenta, nil)
-        FFI::NCurses.addstr(top_bar)
-        Colors.reset!
+        controller.draw_tab_bar('[ session 1 ]')
+        controller.draw_status_bar
         # TODO: move to subview
-        i = 1
-        controller.instance_variable_get(:@logs).each do |e|
-          color = case e[:l]
-                  when :verbose; :nocolor
-                  when :info; :cyan_on_black
-                  when :warn; :yellow_on_black
-                  when :err; :red_on_black
-                  end
-          char = case e[:l]
-                 when :verbose; 'V'
-                 when :info; 'I'
-                 when :warn; 'W'
-                 when :err; 'E'
-                 end
-
-          lines = e[:m].split("\n") # TODO: split on lines that are too long...
-          # draw label
-          FFI::NCurses.move(rect[:y] + i, rect[:x])
-          Colors.with(color) do
-            FFI::NCurses.addch(char.ord)
-          end
-          lines.each_with_index do |l,j|
-            FFI::NCurses.move(rect[:y] + i + j, rect[:x] + 2)
-            Colors.with(color) do
-              FFI::NCurses.addch('|'.ord)
-            end
-            FFI::NCurses.move(rect[:y] + i + j, rect[:x] + 4)
-            FFI::NCurses.addstr(l)
-          end
-          i += lines.count + 1
-        end
+        controller.draw_content
       end
 
       @fetcher_thread = Thread.new do
@@ -67,6 +33,54 @@ module Azurill
         end
       end
 
+    end
+
+    def draw_tab_bar(t)
+      rect = @main_view.rect
+      FFI::NCurses.move(rect[:y], rect[:x])
+      top_bar = t
+      top_bar << (rect[:w] - top_bar.length).times.map {|_| ' ' }.join('')
+      FFI::NCurses.attr_set(FFI::NCurses::A_BOLD, Colors.black_on_magenta, nil)
+      FFI::NCurses.addstr(top_bar)
+      Colors.reset!
+    end
+
+    def draw_status_bar
+    end
+
+    def draw_content
+      rect = @main_view.rect
+      i = 1
+      @logs.each do |e|
+        color = case e[:l]
+                when :verbose; :nocolor
+                when :info; :cyan_on_black
+                when :warn; :yellow_on_black
+                when :err; :red_on_black
+                end
+        char = case e[:l]
+               when :verbose; 'V'
+               when :info; 'I'
+               when :warn; 'W'
+               when :err; 'E'
+               end
+
+        lines = e[:m].split("\n") # TODO: split on lines that are too long...
+        # draw label
+        FFI::NCurses.move(rect[:y] + i, rect[:x])
+        Colors.with(color) do
+          FFI::NCurses.addch(char.ord)
+        end
+        lines.each_with_index do |l,j|
+          FFI::NCurses.move(rect[:y] + i + j, rect[:x] + 2)
+          Colors.with(color) do
+            FFI::NCurses.addch('|'.ord)
+          end
+          FFI::NCurses.move(rect[:y] + i + j, rect[:x] + 4)
+          FFI::NCurses.addstr(l)
+        end
+        i += lines.count + 1
+      end
     end
 
     def handle_char(c)
